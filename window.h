df77ff96933248d4067bcb0bc63a8b087cd42c3f
André Aubin <andre.aubin@lambdaweb.fr>
2013-09-04 12:14:52 +0200

    Adding main widgets to template files (class descriptor)

--- window.h
+++ window.h
@@ -3,21 +3,26 @@
 
 #include <QtWidgets>
 #include "textarea.h"
+#include "yagacore.h"
 
 class Window : public QWidget
 {
 		Q_OBJECT
 	public:
-		explicit Window(QWidget *parent = 0);
-		void    initialize(void);
+		explicit		Window(QWidget *parent = 0);
+		void			initialize(void);
 
 	private:
-		QGridLayout *layout;
-		TextArea *textArea;
+		QGridLayout		*layout;
+		TextArea		*textArea;
+		QLabel			*response;
+		QPushButton		*submitButton;
+		YagaCore		*core;
 
 	signals:
 
 	public slots:
+		void	submitAction();
 
 };
 
