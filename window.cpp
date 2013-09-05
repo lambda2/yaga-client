5ea69bd411d24e815d07761dab19032746d0b6c5
André Aubin <andre.aubin@lambdaweb.fr>
2013-09-05 12:15:50 +0200

    Adding initialization of main widgets. An empty windows shows correctly and the main SIGNAL is properly set

--- window.cpp
+++ window.cpp
@@ -8,9 +8,27 @@ Window::Window(QWidget *parent) :
 
 void    Window::initialize()
 {
+	this->core = new YagaCore();
 	layout = new QGridLayout(this);
 	this->setLayout(layout);
-	   textArea = new TextArea(this);
-	   layout->addWidget(textArea,0,0);
-	   textArea->show();
+
+	textArea = new TextArea(this);
+	layout->addWidget(textArea, 0, 0);
+
+	response = new QLabel(this);
+	layout->addWidget(response, 1, 0);
+
+	submitButton = new QPushButton("Envoyer", this);
+	layout->addWidget(submitButton, 2, 0);
+	this->connect(
+				this->submitButton, SIGNAL(clicked()),
+				this, SLOT(submitAction()));
+}
+
+void	Window::submitAction()
+{
+	QString res;
+
+	res = this->core->digestString(this->textArea->toPlainText());
+	this->response->setText(res);
 }
